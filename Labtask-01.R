b81cdec6f020891e6dbd4e2d356be00eb0eedb7c
Mahir Shariar Ratul <mahirratul2002@gmail.com>
2025-05-01 19:01:08 +0600

    Add files via upload

---
+++ Labtask-01.R
@@ -0,0 +1,20 @@
+ 1 + 1 	
+ 4 + 4
+ 2 + 3 * 4 
+ 5 + 2 * 5
+  
+ sqrt(10) 
+ pi 
+ x <- 3 
+ y <- 4
+ z <- 5
+ x * y * z
+
+ x <- 10
+ x
+ print(x)
+
+ str1 <- "Data"
+ str2 <- "Science"
+ paste(str1, str2) 
+ 
